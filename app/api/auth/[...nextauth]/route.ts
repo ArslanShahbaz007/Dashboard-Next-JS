f5cd53a08dee930c70e0526ccfe9b360a5e763bc
ArslanShahbaz007 <ranaarslanshahbaz@gmail.com>
2025-05-22 06:06:10 -0700

    a

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,47 +1,9 @@
 // app/api/auth/[...nextauth]/route.ts
 
-import NextAuth from 'next-auth';
-import Credentials from 'next-auth/providers/credentials';
 import { authConfig } from '@/auth.config';
-import { z } from 'zod';
-import bcrypt from 'bcryptjs';
-import postgres from 'postgres';
-import type { User } from '@/app/lib/definitions';
+import NextAuth from 'next-auth';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const { auth } = NextAuth(authConfig);
 
-async function getUser(email: string): Promise<User | undefined> {
-  try {
-    const users = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
-    return users[0];
-  } catch (error) {
-    console.error('Failed to fetch user:', error);
-    throw new Error('Failed to fetch user.');
-  }
-}
-
-const handler = NextAuth({
-  ...authConfig,
-  providers: [
-    Credentials({
-      async authorize(credentials) {
-        const parsed = z
-          .object({ email: z.string().email(), password: z.string().min(6) })
-          .safeParse(credentials);
-
-        if (!parsed.success) return null;
-
-        const { email, password } = parsed.data;
-        const user = await getUser(email);
-        if (!user) return null;
-
-        const passwordsMatch = await bcrypt.compare(password, user.password);
-        if (!passwordsMatch) return null;
-
-        return user;
-      },
-    }),
-  ],
-});
-
-export { handler as GET, handler as POST };
+export const GET = auth;
+export const POST = auth;
