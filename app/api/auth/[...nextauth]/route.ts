18e112715c7af81ba8c015b695a79a625125f370
ArslanShahbaz007 <ranaarslanshahbaz@gmail.com>
2025-05-22 06:09:20 -0700

    a

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,9 +0,0 @@
-// app/api/auth/[...nextauth]/route.ts
-
-import { authConfig } from '@/auth.config';
-import NextAuth from 'next-auth';
-
-const { auth } = NextAuth(authConfig);
-
-export const GET = auth;
-export const POST = auth;
